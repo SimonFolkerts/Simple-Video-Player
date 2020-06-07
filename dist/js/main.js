6f1c4d13c3e70c2009ca9f22a1d85e859db6bf8c
SimonFolkerts <simfolkerts@gmail.com>
2020-06-08 10:53:55 +1200

    Viewing video functionality added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,12 @@
 const el_videoList = $('#video-thumbnails');
 
+// INITIALISE ----------------------------------------------------------------
 function init() {
   $.getJSON('json/videos.json', function (data) {
     var videosArray = data.videos;
     displayVideos(videosArray);
   });
 }
-
 
 // DISPLAY VIDEOS ----------------------------------------------------------------
 function displayVideos(videos) {
@@ -15,11 +15,12 @@ function displayVideos(videos) {
     html += makeVideoHTML(videos[i]);
   }
   el_videoList.html(html);
+  addThumbnailClickListener();
 }
 
 function makeVideoHTML(videoObject) {
   return `
-    <div class="video">
+    <div class="video" data-id="${videoObject.id}">
       <img src="https://img.youtube.com/vi/${videoObject.id}/mqdefault.jpg" class="video__thumbnail" alt="${videoObject.title}">
       <h3 class="video__title">${videoObject.title}</h3>
       <p class="video__category">${videoObject.category}</p>
@@ -27,6 +28,26 @@ function makeVideoHTML(videoObject) {
 
 }
 
+function addThumbnailClickListener() {
+  $('.video').click(function () {
+    var videoId = $(this).data('id');
+    playVideo(videoId);
+  });
+}
+
+// VIEW VIDEO ----------------------------------------------------------------
+
+function playVideo(videoId) {
+  var videoUrl = "https://www.youtube.com/embed/" + videoId;
+  $('#player__frame').attr('src', videoUrl);
+  $('#player').show();
+}
+
 
 // RUN ----------------------------------------------------------------
 init();
+$('#player').hide();
+$('#player').click(function() {
+  $('#player__frame').attr('src', '');
+  $('#player').hide();
+});
